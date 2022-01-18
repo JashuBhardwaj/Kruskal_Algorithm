77bd5ef91dd41c3686bce439254a2af0f2538b44
Jashu Bhardwaj <74259821+JashuBhardwaj@users.noreply.github.com>
2022-01-18 13:45:41 +0530

    Test class for Minimun Spanning Tree

---
+++ MSTTest.java
@@ -0,0 +1,39 @@
+// Test class for Minimun Spanning Tree
+
+
+
+public class MSTTest {
+
+	public static void main(String[] args) {
+		
+        WeightedGraphs weight = new WeightedGraphs(9);
+		
+		weight.add(0, 1, 4);
+		weight.add(0, 7, 8);
+		weight.add(1, 2, 8);
+		weight.add(1, 7, 11);
+		weight.add(2, 8, 2);
+		weight.add(3, 4, 9);
+		weight.add(4, 5, 10);
+		weight.add(5, 6, 2);
+		weight.add(6, 7, 1);
+		weight.add(7, 8, 7);
+		weight.add(6, 8, 6);
+		weight.add(2, 5, 4);
+		weight.add(3, 5, 14);
+		weight.add(2, 3, 7);
+		
+		System.out.println("Edges = " + weight.E());
+		
+		System.out.println("Vertices = " + weight.V());
+		
+		KruskalMST mst = new KruskalMST(weight);
+		
+		for(Edge e : mst.edges()) {
+			
+			System.out.println(e.first + "---" + e.weight + "---" + e.second);
+		}
+
+	}
+
+}
